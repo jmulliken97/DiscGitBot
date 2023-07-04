56db67110921cfe959c7c542c86eccbe92bc7d52
jmulliken97 <jmulliken3@gmail.com>
2023-07-04 02:15:31 -0500

    Bot now closes threads when issues are closed.
    Can't sleep this makes this bot just a bit better even thought it wasn't very handy to begin with.

--- bot.js
+++ bot.js
@@ -7,12 +7,12 @@ const client = new Discord.Client({ intents: ["Guilds", "GuildMessages"] });
 
 const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN });
 
-let knownIssues = new Set();
+let knownIssues = new Map();
 
 // Load known issues from file
-if (fs.existsSync('knownIssues.txt')) {
-    const issueIds = fs.readFileSync('knownIssues.txt', 'utf-8').split('\n');
-    knownIssues = new Set(issueIds);
+if (fs.existsSync('knownIssues.json')) {
+    const issues = JSON.parse(fs.readFileSync('knownIssues.json', 'utf-8'));
+    knownIssues = new Map(Object.entries(issues));
 }
 
 client.once('ready', async () => {
@@ -29,22 +29,42 @@ client.once('ready', async () => {
             });
 
             for (const issue of data) {
-                if (knownIssues.has(issue.id)) continue;
-
-                // Add issue to known issues and save to file
-                knownIssues.add(issue.id);
-                fs.appendFileSync('knownIssues.txt', `${issue.id}\n`);
+                if (knownIssues.has(issue.id.toString()) && knownIssues.get(issue.id.toString()).state === issue.state) continue;
 
                 const textChannel = await client.channels.fetch(process.env.DISCORD_CHANNEL_ID);
-                const thread = await textChannel.threads.create({
-                    name: `Issue: ${issue.title}`,
-                    autoArchiveDuration: 1440,
-                    reason: `Discussion for issue: ${issue.title}`,
-                });
+                
+                if (!knownIssues.has(issue.id.toString())) {
+                    // New issue, create thread and save to known issues
+                    const thread = await textChannel.threads.create({
+                        name: `Issue: ${issue.title}`,
+                        autoArchiveDuration: 1440,
+                        reason: `Discussion for issue: ${issue.title}`,
+                    });
+                    await thread.send(`Issue: ${issue.title}\nURL: ${issue.html_url}\nDescription:\n${issue.body}`);
+                    console.log(`Created thread: ${thread.name}`);
 
-                await thread.send(`Issue: ${issue.title}\nURL: ${issue.html_url}\nDescription:\n${issue.body}`);
-                console.log(`Created thread: ${thread.name}`);
+                    // Add issue to known issues and save to file
+                    knownIssues.set(issue.id.toString(), { state: issue.state, threadId: thread.id });
+                } else {
+                    // Known issue, check if status has changed
+                    const issueData = knownIssues.get(issue.id.toString());
+                    if (issueData.state !== issue.state && issue.state === 'closed') {
+                        // Issue is closed, archive thread
+                        const thread = textChannel.threads.cache.get(issueData.threadId);
+                        if (thread) {
+                            await thread.setArchived(true);
+                            console.log(`Archived thread: ${thread.name}`);
+                        }
+                        
+                        // Update issue status in known issues
+                        issueData.state = issue.state;
+                        knownIssues.set(issue.id.toString(), issueData);
+                    }
+                }
             }
+
+            // Save known issues to file
+            fs.writeFileSync('knownIssues.json', JSON.stringify(Object.fromEntries(knownIssues)));
         } catch (error) {
             console.error(`Error: ${error}`);
         }
@@ -52,4 +72,3 @@ client.once('ready', async () => {
 });
 
 client.login(process.env.DISCORD_TOKEN);
-
